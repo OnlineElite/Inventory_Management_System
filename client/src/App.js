666ace0254d90300d745092b3545d89a6d8ca013
OnlineElite <online.0.elite@gmail.com>
2023-08-29 00:11:35 +0100

    adding table product

--- client/src/App.js
+++ client/src/App.js
@@ -22,19 +22,19 @@ function App(props) {
 
   return (
     <div>
-      {/*<Dashboard/>*/}
-        <Routes>
-          <Route exact path='/' element={<LandingPage/>} />
-          <Route path='/register' element={<RegisterForm/>} />
-          <Route path='/login' element={<LoginForm/>} />
-          <Route path="/userInterface" element={<UserInterface/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/dashboard"
-            element={<PrivateRoute element={<Dashboard/>} 
-              isAuthenticated={props.isAuthenticated} isAdmin={props.isAdmin}
-            />}
-          />
-        </Routes>
+      <Dashboard/>
+      {/*<Routes>
+        <Route exact path='/' element={<LandingPage/>} />
+        <Route path='/register' element={<RegisterForm/>} />
+        <Route path='/login' element={<LoginForm/>} />
+        <Route path="/userInterface" element={<UserInterface/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard"
+          element={<PrivateRoute element={<Dashboard/>} 
+            isAuthenticated={props.isAuthenticated} isAdmin={props.isAdmin}
+          />}
+        />
+      </Routes>*/}
     </div>
   );
 }
