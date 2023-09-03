7c239a4d3fcf96e217ce26ecfea541644fa267be
OnlineElite <online.0.elite@gmail.com>
2023-09-03 17:13:12 +0100

    adding description column to stock table

--- Server/models/Products.js
+++ Server/models/Products.js
@@ -7,9 +7,10 @@ class Product {
       products.ref as product_ref, 
       products.stock as product_stock,
       products.price as product_price,
+      products.Description as product_desc,
       categories.name as category_name, 
       brands.name as brand_name
-      from products 
+      from products
       inner join categories on categories.id = products.category_id
       inner join brands on brands.id = products.brand_id`;
       const result = await pool.query(query);
@@ -31,9 +32,9 @@ class Product {
 
 class ProductAction {
   static async addProduct(product){
-    const query = `insert into products (name , ref, stock, price, category_id, brand_id)
+    const query = `insert into products (name , ref, stock, price, Description, category_id, brand_id)
     values ('${product.product_name}', '${product.product_ref}', ${product.product_stock}, 
-    ${product.product_price}, ${product.category_name}, ${product.brand_name})`
+    ${product.product_price}, '${product.product_desc}', ${product.category_name}, ${product.brand_name})`
 
     const result = await pool.query(query);
     return result.rows;
@@ -48,7 +49,7 @@ class ProductAction {
 
   static async updateProduct(product){
     const query = `update products set name = '${product.product_name}',ref = '${product.product_ref}',
-     stock = ${product.product_stock}, price = ${product.product_price}, category_id = ${product.category_name},
+     stock = ${product.product_stock}, price = ${product.product_price}, Description =  '${product.product_desc}', category_id = ${product.category_name},
     brand_id = ${product.brand_name} where ref = '${product.condition}'`
 
     const result = await pool.query(query);
