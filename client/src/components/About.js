10e34142bca27400b53a6ffae6a563526fc12501
OnlineElite <online.0.elite@gmail.com>
2023-09-20 00:27:43 +0100

    adding contact page

---
+++ client/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About(){
+
+
+    return(
+        <div id='about'>
+            <h1>welcome on about page</h1>
+        </div>
+    )
+}
+
+export default About;
