e9528abbfabdf266e3d11a7ba06b76698a0335f3
OnlineElite <online.0.elite@gmail.com>
2023-09-23 19:41:06 +0100

    last update

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,10 +1,11 @@
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Inventory",
-  password: "root",
+  user: "elite",
+  host: "dpg-ck7isenq54js73ftogkg-a.frankfurt-postgres.render.com",
+  database: "inventory_y8fi",
+  password: "GMD2koJ4rbVIbagvl4apzdtMagsrUc0Q",
   port: 5432,
 });
 
