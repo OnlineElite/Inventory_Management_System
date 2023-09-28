3f2f8c312ad35f648f07b7581c820abe373881b7
OnlineElite <online.0.elite@gmail.com>
2023-09-28 14:55:31 +0100

    stiling description

--- client/src/components/UserInterface.js
+++ client/src/components/UserInterface.js
@@ -196,19 +196,19 @@ function UserInterface(props){
                             alt="product"
                           />
                           <div className="card-body">
-                            <div className="lines">
+                            <div className="line_desc">
                               {/*<span className="detail">Reference:</span>*/}
-                              <span className="result">{product.product_name} {product.product_desc} </span>
+                              <span className="desc">{product.product_name} {product.product_desc} </span>
                             </div>
-                            <div className="lines">
+                            <div className="line_prices ">
                               {/*<span className="detail">Brand:</span>
                               <span className="result"> {product.brand_name} </span>*/}
-                              <span className='result text-black font-weight-bold'> {((product.product_price)-(product.product_price)*20/100).toFixed(2)+'DH'}</span>
+                              <span className='prices '> {((product.product_price)-(product.product_price)*20/100).toFixed(2)+'DH'}</span>
                             </div>
-                            <div className="lines">
+                            <div className="line_old_price">
                               {/*<span className="detail">Name:</span>
                               <span className="result"> {product.product_name} </span>*/}
-                              <span className="result text-muted"> {" "}{product.product_price}DH{" "} </span>
+                              <span className="old_price"> {" "}{product.product_price}DH{" "} </span>
                             </div>
                            { /*<div className="lines">
                               <span className="detail">Quantity:</span>
