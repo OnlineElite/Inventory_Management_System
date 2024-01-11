c3ab4dc5a44cd854df7fb7274dfaf23b0d299d04
OnlineElite <online.0.elite@gmail.com>
2024-01-11 20:02:45 +0100

    add vercel database

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,5 +1,7 @@
-/*const { Pool } = require("pg");
+const { Pool } = require("pg");
 
+// Local Test
+/* 
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -7,17 +9,26 @@ const pool = new Pool({
   password: "Elite%159",
   port: 5432,
 });
-
-module.exports = pool
 */
 
-const { Pool } = require("pg");
-
+// Rander
+/* 
 const pool = new Pool({
   user: "elite",
   host: "dpg-ck7isenq54js73ftogkg-a.frankfurt-postgres.render.com",
   database: "inventory_y8fi",
   password: "GMD2koJ4rbVIbagvl4apzdtMagsrUc0Q",
+  port: 5432,
+  ssl: true,
+});
+*/
+
+// Vercel
+const pool = new Pool({
+  user: "default",
+  host: "ep-green-sound-29068208-pooler.ap-southeast-1.postgres.vercel-storage.com",
+  database: "verceldb",
+  password: "DdyLse3oP2qc",
   port: 5432,
   ssl: true,
 });
